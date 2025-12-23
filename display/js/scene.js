b9a64ec477a82de7debeb12159fa4a1778a1d762
Andrish97 <andrew.shum1997@gmail.com>
2025-12-23 02:08:55 +0100

    Update scene.js

--- display/js/scene.js
+++ display/js/scene.js
@@ -645,7 +645,7 @@ export async function createScene() {
   const lampBX = barX + barW - padX; // B po prawej
   
   const lampA = makeLamp(basebar, lampAX, lampsY, lampR, "#ff2e3b");
-  const lampB = makeLamp(basebar, lampBX, lampsY, lampR, "#2a62ff");
+  const lampB = makeLamp(basebar, lampBX, lampsY, lampR, "#4db4ff");
   
   lampA.setOn(false);
   lampB.setOn(false);
