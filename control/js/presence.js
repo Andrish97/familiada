be9724a2067057c939c0acf4a101a8b941980a89
Andrish97 <andrew.shum1997@gmail.com>
2025-12-28 18:16:34 +0100

    Update presence.js

--- control/js/presence.js
+++ control/js/presence.js
@@ -86,16 +86,17 @@ export function createPresence({ game, ui, store, devices }) {
         }
       })();
     }
+  }
 
-    async function start() {
-      await tick();
-      timer = setInterval(tick, 1500);
-    }
-  
-    function stop() {
-      if (timer) clearInterval(timer);
-      timer = null;
-    }
+  async function start() {
+    await tick();
+    timer = setInterval(tick, 1500);
+  }
+
+  function stop() {
+    if (timer) clearInterval(timer);
+    timer = null;
+  }
   
     return { start, stop };
 }
