240f167f50f03b758e2dfc64670cd69b605aea0a
Andrish97 <andrew.shum1997@gmail.com>
2026-04-10 05:28:20 +0200

    Update settings.js

--- js/pages/settings.js
+++ js/pages/settings.js
@@ -12,7 +12,7 @@ Settings panel (admin)
 - GET /_admin_api/mail/logs
 */
 
-import { initI18n, t } from "../../translation/translation.js?v=v2026-04-10T03161";
+import { initI18n, t, getUiLang } from "../../translation/translation.js?v=v2026-04-10T03161";
 import { initUiSelect } from "../core/ui-select.js?v=v2026-04-10T03161";
 import { confirmModal } from "../core/modal.js?v=v2026-04-10T03161";
 import { sb } from "../core/supabase.js?v=v2026-04-10T03161";
