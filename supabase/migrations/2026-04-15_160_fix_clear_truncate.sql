d7a49878f1522036f1bc3f1f3b4f9e1f35f72221
Andrish97 <andrew.shum1997@gmail.com>
2026-04-15 20:19:59 +0200

    fix: migration 160 - use TRUNCATE in clear functions

---
+++ supabase/migrations/2026-04-15_160_fix_clear_truncate.sql
@@ -0,0 +1,26 @@
+-- Migration: Fix clear functions to use TRUNCATE
+
+CREATE OR REPLACE FUNCTION public.clear_marketing_search_logs()
+RETURNS void
+LANGUAGE plpgsql
+SECURITY DEFINER
+SET search_path = public
+AS $$
+BEGIN
+    EXECUTE 'TRUNCATE TABLE marketing_search_logs CASCADE';
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION public.clear_marketing_queries_log()
+RETURNS void
+LANGUAGE plpgsql
+SECURITY DEFINER
+SET search_path = public
+AS $$
+BEGIN
+    EXECUTE 'TRUNCATE TABLE marketing_search_queries_log CASCADE';
+END;
+$$;
+
+GRANT EXECUTE ON FUNCTION public.clear_marketing_search_logs() TO service_role;
+GRANT EXECUTE ON FUNCTION public.clear_marketing_queries_log() TO service_role;
