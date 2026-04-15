0d716eb3748314b95d9e34fd25e8e06812560faf
Andrish97 <andrew.shum1997@gmail.com>
2026-04-15 20:07:23 +0200

    fix: add migration 159 with clear functions for marketing tables

---
+++ supabase/migrations/2026-04-15_159_fix_clear_functions.sql
@@ -0,0 +1,26 @@
+-- Migration: Create/fix clear functions for marketing tables
+
+CREATE OR REPLACE FUNCTION public.clear_marketing_search_logs()
+RETURNS void
+LANGUAGE plpgsql
+SECURITY DEFINER
+SET search_path = public
+AS $$
+BEGIN
+    DELETE FROM marketing_search_logs;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION public.clear_marketing_queries_log()
+RETURNS void
+LANGUAGE plpgsql
+SECURITY DEFINER
+SET search_path = public
+AS $$
+BEGIN
+    DELETE FROM marketing_search_queries_log;
+END;
+$$;
+
+GRANT EXECUTE ON FUNCTION public.clear_marketing_search_logs() TO service_role;
+GRANT EXECUTE ON FUNCTION public.clear_marketing_queries_log() TO service_role;
